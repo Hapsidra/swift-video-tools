e3858976d0b4f1314a2d8a2ff8c3ba57f06d084a
Максим Ефимов <hapsidra@outlook.com>
2018-05-26 17:36:24 +0900

    refactoring

--- Sources/VideoTools.swift
+++ Sources/VideoTools.swift
@@ -3,28 +3,25 @@
 //
 
 import AVFoundation
-import AssetsLibrary
-import Photos
 
-public func processVideo(url: URL, processLayer: @escaping (CALayer) -> (), completion: @escaping (URL) -> ()) {
+public func editVideoLayer(url: URL, presetName: String = AVAssetExportPresetMediumQuality, processLayer: @escaping (CALayer) -> (), completion: @escaping (URL) -> ()) {
     print(#function)
     let asset = AVAsset(url: url)
     let composition = AVMutableComposition()
-    let compositionVideoTrack = composition.addMutableTrack(withMediaType: AVMediaType.video, preferredTrackID: kCMPersistentTrackID_Invalid)
-    let compositionAudioTrack = composition.addMutableTrack(withMediaType: AVMediaType.audio, preferredTrackID: kCMPersistentTrackID_Invalid)
+    let compositionVideoTrack = composition.addMutableTrack(withMediaType: AVMediaType.video, preferredTrackID: kCMPersistentTrackID_Invalid)!
+    let compositionAudioTrack = composition.addMutableTrack(withMediaType: AVMediaType.audio, preferredTrackID: kCMPersistentTrackID_Invalid)!
     let videoTrack = asset.tracks(withMediaType: AVMediaType.video).first!
     let audioTrack = asset.tracks(withMediaType: AVMediaType.audio).first!
     do {
-        try compositionVideoTrack?.insertTimeRange(CMTimeRangeMake(kCMTimeZero, asset.duration), of: videoTrack, at: kCMTimeZero)
-        try compositionAudioTrack?.insertTimeRange(CMTimeRangeMake(kCMTimeZero, asset.duration), of: audioTrack, at: kCMTimeZero)
+        try compositionVideoTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, asset.duration), of: videoTrack, at: kCMTimeZero)
+        try compositionAudioTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, asset.duration), of: audioTrack, at: kCMTimeZero)
     } catch {
         print(error)
     }
 
-    compositionVideoTrack?.preferredTransform = videoTrack.preferredTransform
+    compositionVideoTrack.preferredTransform = videoTrack.preferredTransform
 
     var videoSize = videoTrack.naturalSize.applying(videoTrack.preferredTransform); videoSize.width = abs(videoSize.width); videoSize.height = abs(videoSize.height)
-    print("size of video:", videoSize.width, videoSize.height)
 
     let parentLayer = CALayer()
     parentLayer.frame = CGRect(x: 0, y: 0, width: videoSize.width, height: videoSize.height)
@@ -47,9 +44,9 @@ public func processVideo(url: URL, processLayer: @escaping (CALayer) -> (), comp
     let instruction = AVMutableVideoCompositionInstruction()
     instruction.timeRange = CMTimeRangeMake(kCMTimeZero, composition.duration)
 
-    let resultVideoTrack = composition.tracks(withMediaType: AVMediaType.video).first! //may be compositionVideoTrack
-    let layerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: resultVideoTrack)
-    layerInstruction.setTransform(resultVideoTrack.preferredTransform, at: kCMTimeZero)
+    //let resultVideoTrack = composition.tracks(withMediaType: AVMediaType.video).first! //may be compositionVideoTrack
+    let layerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: compositionVideoTrack)
+    layerInstruction.setTransform(compositionVideoTrack.preferredTransform, at: kCMTimeZero)
 
     instruction.layerInstructions = [layerInstruction]
     videoComposition.instructions = [instruction]
@@ -64,7 +61,7 @@ public func processVideo(url: URL, processLayer: @escaping (CALayer) -> (), comp
         }
     }
 
-    let exportSession = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetMediumQuality)!
+    let exportSession = AVAssetExportSession(asset: composition, presetName: presetName)!
     exportSession.videoComposition = videoComposition
     exportSession.outputURL = outputUrl
     exportSession.outputFileType = AVFileType.mp4
@@ -72,7 +69,6 @@ public func processVideo(url: URL, processLayer: @escaping (CALayer) -> (), comp
 
     exportSession.exportAsynchronously(completionHandler: {
         if exportSession.status == AVAssetExportSessionStatus.completed {
-            print("complete")
             completion(outputUrl)
         } else {
             print("export error")
