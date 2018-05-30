558dcf440444f943edd58fd284e9f8f1b7102a36
Максим Ефимов <hapsidra@outlook.com>
2018-05-30 12:22:25 +0900

    background music added

--- Sources/VideoTools.swift
+++ Sources/VideoTools.swift
@@ -4,7 +4,7 @@
 
 import AVFoundation
 
-public func editVideoLayer(url: URL, presetName: String = AVAssetExportPresetMediumQuality, processLayer: @escaping (CALayer) -> (), completion: @escaping (URL) -> ()) {
+public func editVideoLayer(url: URL, musicURL: URL? = nil, presetName: String = AVAssetExportPresetMediumQuality, processLayer: @escaping (CALayer) -> (), completion: @escaping (URL) -> ()) {
     print(#function)
     let asset = AVAsset(url: url)
     let composition = AVMutableComposition()
@@ -12,45 +12,62 @@ public func editVideoLayer(url: URL, presetName: String = AVAssetExportPresetMed
     let compositionAudioTrack = composition.addMutableTrack(withMediaType: AVMediaType.audio, preferredTrackID: kCMPersistentTrackID_Invalid)!
     let videoTrack = asset.tracks(withMediaType: AVMediaType.video).first!
     let audioTrack = asset.tracks(withMediaType: AVMediaType.audio).first!
+    
     do {
         try compositionVideoTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, asset.duration), of: videoTrack, at: kCMTimeZero)
         try compositionAudioTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, asset.duration), of: audioTrack, at: kCMTimeZero)
     } catch {
         print(error)
     }
-
+    
+    if musicURL != nil {
+        let compositionMusicTrack = composition.addMutableTrack(withMediaType: AVMediaType.audio, preferredTrackID: kCMPersistentTrackID_Invalid)!
+        let musicAsset = AVAsset(url: musicURL!)
+        let musicAudioTrack = musicAsset.tracks(withMediaType: .audio).first!
+        do {
+            let repeatCount = Int(ceil(asset.duration.seconds / musicAsset.duration.seconds))
+            for i in 0..<repeatCount {
+                let duration = CMTime(seconds: min(musicAsset.duration.seconds, asset.duration.seconds - Double(i) * musicAsset.duration.seconds), preferredTimescale: 1000)
+                let at = CMTime(seconds: Double(i) * musicAsset.duration.seconds, preferredTimescale: 1000)
+                let range = CMTimeRangeMake(kCMTimeZero, duration)
+                try compositionMusicTrack.insertTimeRange(range, of: musicAudioTrack, at: at)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
     compositionVideoTrack.preferredTransform = videoTrack.preferredTransform
-
+    
     var videoSize = videoTrack.naturalSize.applying(videoTrack.preferredTransform); videoSize.width = abs(videoSize.width); videoSize.height = abs(videoSize.height)
-
+    
     let parentLayer = CALayer()
     parentLayer.frame = CGRect(x: 0, y: 0, width: videoSize.width, height: videoSize.height)
-
+    
     let videoLayer = CALayer()
     videoLayer.frame = CGRect(x: 0, y: 0, width: videoSize.width, height: videoSize.height)
     parentLayer.addSublayer(videoLayer)
-
+    
     let customizableLayer = CALayer()
     customizableLayer.frame = CGRect(x: 0, y: 0, width: videoSize.width, height: videoSize.height)
     customizableLayer.masksToBounds = true
     processLayer(customizableLayer)
     parentLayer.addSublayer(customizableLayer)
-
+    
     let videoComposition = AVMutableVideoComposition()
     videoComposition.frameDuration = CMTimeMake(1, 30)
     videoComposition.renderSize = videoSize
     videoComposition.animationTool = AVVideoCompositionCoreAnimationTool(postProcessingAsVideoLayer: videoLayer, in: parentLayer)
-
+    
     let instruction = AVMutableVideoCompositionInstruction()
     instruction.timeRange = CMTimeRangeMake(kCMTimeZero, composition.duration)
-
-    //let resultVideoTrack = composition.tracks(withMediaType: AVMediaType.video).first! //may be compositionVideoTrack
+    
     let layerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: compositionVideoTrack)
     layerInstruction.setTransform(compositionVideoTrack.preferredTransform, at: kCMTimeZero)
-
+    
     instruction.layerInstructions = [layerInstruction]
     videoComposition.instructions = [instruction]
-
+    
     let outputUrl = URL(fileURLWithPath: NSTemporaryDirectory().appending(UUID().uuidString.appending(".mp4")))
     if FileManager.default.fileExists(atPath: outputUrl.path) {
         do {
@@ -60,13 +77,13 @@ public func editVideoLayer(url: URL, presetName: String = AVAssetExportPresetMed
             print(error)
         }
     }
-
+    
     let exportSession = AVAssetExportSession(asset: composition, presetName: presetName)!
     exportSession.videoComposition = videoComposition
     exportSession.outputURL = outputUrl
     exportSession.outputFileType = AVFileType.mp4
     exportSession.shouldOptimizeForNetworkUse = true
-
+    
     exportSession.exportAsynchronously(completionHandler: {
         if exportSession.status == AVAssetExportSessionStatus.completed {
             completion(outputUrl)
